14129cee7c171d872fd9d28d055e1c716b89abbf
Bob Brown <bobbrow@microsoft.com>
2016-01-21 10:08:03 -0800

    Add MIT license to d3dx12.h: issue #77

--- Samples/D3D12ExecuteIndirect/src/d3dx12.h
+++ Samples/D3D12ExecuteIndirect/src/d3dx12.h
@@ -1,11 +1,13 @@
-//////////////////////////////////////////////////////////////////////////////
+//*********************************************************
 //
-//  Copyright (C) Microsoft Corporation.  All Rights Reserved.
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
-//  File:       d3dx12.h
-//  Content:    D3DX12 utility library
-//
-//////////////////////////////////////////////////////////////////////////////
+//*********************************************************
 
 #ifndef __D3DX12_H__
 #define __D3DX12_H__
