1cf295dafbac7a92e354268488fa6ef8ef21fad1
Christopher Wallis <chwallis@ntdev.microsoft.com>
2018-03-26 16:50:46 -0700

    Adding prebuild check

---
+++ Samples/Desktop/D3D12Raytracing/tools/x64/PrebuildCheck.bat
@@ -0,0 +1,21 @@
+set FILENOTFOUND=0
+if not exist dxcompiler.dll (
+   echo No dxcompiler.dll found!
+   set FILENOTFOUND=1
+)
+
+if not exist d3d12.dll (
+   echo No d3d12.dll found!
+   set FILENOTFOUND=1
+)
+
+if not exist dxc.exe (
+   echo No dxc.exe found!
+   set FILENOTFOUND=1
+)
+
+if %FILENOTFOUND% NEQ 0 (
+	echo Please go to https://github.com/Microsoft/DirectX-Graphics-Samples/releases and download "DirectX Raytracing Binary Release v1.0". 
+	echo Copy all the binaries to Samples\Desktop\D3D12Raytracing\tools\x64"
+	exit /b 1
+)
