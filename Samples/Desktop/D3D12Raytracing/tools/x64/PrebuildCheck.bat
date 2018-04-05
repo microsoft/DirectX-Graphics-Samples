f60c880e75ebe406c529965a6ff814859f32544a
Christopher Wallis <chwallis@ntdev.microsoft.com>
2018-04-04 18:33:20 -0700

    Adding a version check that makes sure binaries always match the source

--- Samples/Desktop/D3D12Raytracing/tools/x64/PrebuildCheck.bat
+++ Samples/Desktop/D3D12Raytracing/tools/x64/PrebuildCheck.bat
@@ -1,4 +1,5 @@
 set FILENOTFOUND=0
+
 if not exist dxcompiler.dll (
    echo No dxcompiler.dll found!
    set FILENOTFOUND=1
@@ -28,6 +29,23 @@ copy dxc.exe fxc.exe
 
 if %FILENOTFOUND% NEQ 0 (
 	echo Please go to https://github.com/Microsoft/DirectX-Graphics-Samples/releases and download "DirectX Raytracing Binary Release v1.0". 
+	echo Copy all the binaries to Samples\Desktop\D3D12Raytracing\tools\x64"
+	exit /b 1
+)
+
+if not exist version.txt (
+   goto :mismatch
+) 
+
+set /p version=< version.txt
+if not "%version%"=="1.1-dxr" (
+	goto :mismatch
+)
+
+exit
+
+:mismatch
+	echo Stale binaries detected, please grab the v1.1-dxr binaries from https://github.com/Microsoft/DirectX-Graphics-Samples/releases 
 	echo Copy all the binaries to Samples\Desktop\D3D12Raytracing\tools\x64"
 	exit /b 1
 )
