a80c360866d883ad3da96a1e8b75466242ecc865
Peter Kristof <pkristof@microsoft.com>
2018-04-26 12:56:43 -0700

    Fix up ray sphere intersection tests for metaballs.

--- Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingProceduralGeometry/VolumetricPrimitives.h
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingProceduralGeometry/VolumetricPrimitives.h
@@ -23,10 +23,10 @@ float CalculateMetaballPotential(in float3 position, in float3 mbCenter, in floa
 
     if (d <= mbRadius)
     {
-        //return 2 * (d * d * d) / (mbRadius * mbRadius * mbRadius)
-        //    - 3 * (d * d) / (mbRadius * mbRadius)
-        //    + 1;
-        float dR = d * mbRadius;
+        return 2 * (d * d * d) / (mbRadius * mbRadius * mbRadius)
+            - 3 * (d * d) / (mbRadius * mbRadius)
+            + 1;
+        float dR = d / mbRadius;
         return (2 * dR - 3) * (dR * dR) + 1;
     }
     return 0;
@@ -36,7 +36,7 @@ float CalculateMetaballPotential(in float3 position, in float3 mbCenter, in floa
 // Ref: http://www.geisswerks.com/ryan/BLOBS/blobs.html
 bool RayMetaballsIntersectionTest(in Ray ray, out float thit, out ProceduralPrimitiveAttributes attr, in float totalTime)
 {
-    const int N = 3;
+    const UINT N = 3;
     // ToDo Pass in from the app?
     // Metaball centers at t0 and t1 key frames.
     float3 keyFrameCenters[N][2] =
@@ -54,23 +54,53 @@ bool RayMetaballsIntersectionTest(in Ray ray, out float thit, out ProceduralPrim
     centers[2] = lerp(keyFrameCenters[2][0], keyFrameCenters[2][1], tAnimate);
 
     // Metaball field radii of max influence
-    float radii[N] = { 0.50, 0.65, 0.50 };
+    float radii[N] = { 0.50, 0.65, 0.50 };    // ToDo Compare perf with precomputed invRadius
     
     // Set bounds for ray march to in and out intersection 
     // against max influence of all metaballs. 
     float tmin, tmax;
     tmin = RayTCurrent();
     tmax = RayTMin();
+#if 1
+    float _thit, _tmax;
+#if DO_NOT_USE_DYNAMIC_LOOPS
+    if (RaySolidSphereIntersectionTest(ray, _thit, _tmax, centers[0], radii[0]))
+    {
+        tmin = min(_thit, tmin);
+        tmax = max(_tmax, tmax);
+    }
+    if (RaySolidSphereIntersectionTest(ray, _thit, _tmax, centers[1], radii[1]))
+    {
+        tmin = min(_thit, tmin);
+        tmax = max(_tmax, tmax);
+    }
+    if (RaySolidSphereIntersectionTest(ray, _thit, _tmax, centers[2], radii[2]))
+    {
+        tmin = min(_thit, tmin);
+        tmax = max(_tmax, tmax);
+    }
+#else
     for (UINT j = 0; j < N; j++)
     {
-        float _thit, _tmax;
-        ProceduralPrimitiveAttributes _attr;
         if (RaySphereIntersectionTest(ray, _thit, _tmax, _attr, centers[j], radii[j]))
         {
-            tmin = min(_thit, tmin);
-            tmax = max(_tmax, tmax);
+            tmin = min(_thit - 1, tmin);
+            tmax = max(_tmax + 1, tmax);
         }
     }
+#endif
+#else
+    float3 aabb[2] = {
+        float3(-1,-1,-1),
+        float3(1,1,1)
+    };
+    if (!RayAABBIntersectionTest(ray, aabb, tmin, tmax))
+    {
+        return false;
+    }
+#endif
+    tmin = max(tmin, RayTMin());
+    tmax = min(tmax, RayTCurrent());
 
     UINT MAX_STEPS = 128;
     float tstep = (tmax - tmin) / (MAX_STEPS - 1);
@@ -83,26 +113,43 @@ bool RayMetaballsIntersectionTest(in Ray ray, out float thit, out ProceduralPrim
         float t = tmin + i * tstep;
         float3 position = ray.origin + t * ray.direction;
         float fieldPotentials[N];
+        float fieldPotential = 0;
+#if DO_NOT_USE_DYNAMIC_LOOPS
         fieldPotentials[0] = CalculateMetaballPotential(position, centers[0], radii[0]);
         fieldPotentials[1] = CalculateMetaballPotential(position, centers[1], radii[1]);
         fieldPotentials[2] = CalculateMetaballPotential(position, centers[2], radii[2]);
-        float fieldPotential = fieldPotentials[0] + fieldPotentials[1] + fieldPotentials[2];
-
+        fieldPotential = fieldPotentials[0] + fieldPotentials[1] + fieldPotentials[2];
+#else
+        for (UINT j = 0; j < N; j++)
+        {
+            fieldPotentials[i] = CalculateMetaballPotential(position, centers[i], radii[i]);
+            fieldPotential += fieldPotentials[i];
+        }
+#endif
         // ToDo revise threshold range
         // ToDo pass threshold from app
-        // Threshold - valid range is (0, 0.25>, the larger the threshold the smaller the blob.
-        float threshold = 0.15f;
+        // Threshold - valid range is (0, 0.1>, the larger the threshold the smaller the blob.
+        float threshold = 0.25f;
         if (fieldPotential >= threshold)
         {
             // Calculate normal as a weighted average of sphere normals from contributing metaballs.
             float3 normal = float3(0, 0, 0);
+#if DO_NOT_USE_DYNAMIC_LOOPS
             normal += fieldPotentials[0] * CalculateNormalForARaySphereHit(ray, t, centers[0]);
             normal += fieldPotentials[1] * CalculateNormalForARaySphereHit(ray, t, centers[1]);
             normal += fieldPotentials[2] * CalculateNormalForARaySphereHit(ray, t, centers[2]);
+#else
+            //[unroll]
+            for (UINT j = 0; j < N; j++)
+            {
+                normal += fieldPotentials[j] * CalculateNormalForARaySphereHit(ray, t, centers[j]);
+            }
+#endif
+            normal = normalize(normal/fieldPotential);
             if (IsAValidHit(ray, t, normal))
             {
                 thit = t;
-                attr.normal = normalize(normal / fieldPotential);
+                attr.normal = normal;
                 return true;
             }
         }
