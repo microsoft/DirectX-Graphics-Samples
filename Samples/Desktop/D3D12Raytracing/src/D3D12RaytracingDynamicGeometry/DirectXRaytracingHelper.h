f00afec512ee4234bfd21e376fa29de93a7fd2b9
Peter Kristof <pkristof@microsoft.com>
2018-05-27 14:10:33 -0700

    Fixed input flag not getting applied. Updated DXR feature availability to the latest API change.

--- Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingDynamicGeometry/DirectXRaytracingHelper.h
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingDynamicGeometry/DirectXRaytracingHelper.h
@@ -286,24 +286,16 @@ inline void PrintStateObjectDesc(const D3D12_STATE_OBJECT_DESC* desc)
     OutputDebugStringW(wstr.str().c_str());
 }
 
-
-// Enable experimental features and return if they are supported.
-// To test them being supported we need to check both their enablement as well as device creation afterwards.
-template <std::size_t N>
-inline bool EnableD3D12ExperimentalFeatures(IDXGIAdapter1* adapter, UUID(&experimentalFeatures)[N])
-{
-    ComPtr<ID3D12Device> testDevice;
-    return SUCCEEDED(D3D12EnableExperimentalFeatures(N, experimentalFeatures, nullptr, nullptr))
-        && SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&testDevice)));
-}
-
 // Enable experimental features required for compute-based raytracing fallback.
 // This will set active D3D12 devices to DEVICE_REMOVED state.
 // Returns bool whether the call succeeded and the device supports the feature.
 inline bool EnableComputeRaytracingFallback(IDXGIAdapter1* adapter)
 {
-    UUID experimentalFeatures[] = { D3D12ExperimentalShaderModels };
-    return EnableD3D12ExperimentalFeatures(adapter, experimentalFeatures);
+	ComPtr<ID3D12Device> testDevice;
+	UUID experimentalFeatures[] = { D3D12ExperimentalShaderModels };
+
+	return SUCCEEDED(D3D12EnableExperimentalFeatures(1, experimentalFeatures, nullptr, nullptr))
+		&& SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&testDevice)));
 }
 
 // Enable experimental features required for driver and compute-based fallback raytracing.
@@ -311,8 +303,13 @@ inline bool EnableComputeRaytracingFallback(IDXGIAdapter1* adapter)
 // Returns bool whether the call succeeded and the device supports the feature.
 inline bool EnableRaytracing(IDXGIAdapter1* adapter)
 {
-    UUID experimentalFeatures[] = { D3D12ExperimentalShaderModels, D3D12RaytracingPrototype };
-    return EnableD3D12ExperimentalFeatures(adapter, experimentalFeatures);
+	ComPtr<ID3D12Device> testDevice;
+	ComPtr<ID3D12DeviceRaytracingPrototype> testRaytracingDevice;
+	UUID experimentalFeatures[] = { D3D12ExperimentalShaderModels, D3D12RaytracingPrototype };
+
+	return SUCCEEDED(D3D12EnableExperimentalFeatures(2, experimentalFeatures, nullptr, nullptr))
+		&& SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&testDevice)))
+		&& SUCCEEDED(testDevice->QueryInterface(IID_PPV_ARGS(&testRaytracingDevice)));
 }
 
 inline void StoreXMMatrixAsTransform3x4
