70d55b4f844394bfee0c3d211876a62d4d6e1546
Peter Kristof <pkristof@microsoft.com>
2018-09-11 08:14:49 -0700

    Added check on DXR feature support to Hello World;

--- Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloWorld/DirectXRaytracingHelper.h
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloWorld/DirectXRaytracingHelper.h
@@ -298,10 +298,11 @@ inline bool EnableComputeRaytracingFallback(IDXGIAdapter1* adapter)
 inline bool EnableRaytracing(IDXGIAdapter1* adapter)
 {
     ComPtr<ID3D12Device> testDevice;
-    ComPtr<ID3D12Device5> testRaytracingDevice;
-    UUID experimentalFeatures[] = { D3D12ExperimentalShaderModels};
- 
-    return SUCCEEDED(D3D12EnableExperimentalFeatures(1, experimentalFeatures, nullptr, nullptr))
-        && SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&testDevice)))
-        && SUCCEEDED(testDevice->QueryInterface(IID_PPV_ARGS(&testRaytracingDevice)));
+    D3D12_FEATURE_DATA_D3D12_OPTIONS5 featureSupportData = {};
+
+    bool passed = SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&testDevice)))
+        && SUCCEEDED(testDevice->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS5, &featureSupportData, sizeof(featureSupportData)))
+        && featureSupportData.RaytracingTier == D3D12_RAYTRACING_TIER_1_0;
+
+    return passed;
 }
