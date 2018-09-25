d19459f97db6c48dde1a07415cda19585f1ec5bb
Peter Kristof <pkristof@microsoft.com>
2018-09-25 07:21:49 -0700

    User VS HLSL compiler build step, use dxc binaries from SDK. (#466)
    Removed dependency on private binaries of dxc, dxcompiler and dxil. The ones from SDK are used.
    DXC is now supported as of VS 15.8. With that:
    --- Switched from custom build step to VS HLSL compiler build step for shaders.
    --- Removed dependency on a hack of renaming dxc to fxc. This is not needed anymore as VS now uses dxc for SM 6.0+. Moved Compute Shaders to SM 6.0 so that dxc is used.
    Cleaned up FL and FLunittests vcxproj. There were a lot of incorrect or inconsistent settings, i.e. use of SM 4.0, which were ignored previously, because dxc was being forced.

--- Samples/Desktop/D3D12Raytracing/tools/x64/PostbuildCopy.bat
+++ Samples/Desktop/D3D12Raytracing/tools/x64/PostbuildCopy.bat
@@ -5,7 +5,7 @@ for /F "tokens=3* delims=." %%f in ("%OS%") do ( set OS=%%f )
 set BuildVersion=%OS%
 rem Remove whitespace
 
-rem Copy Dxcompiler, needed regardless of whether it's using native DXR or the Fallback layer
+rem Copy dxrfallbackcompiler.dll needed regardless of whether it's using native DXR or the Fallback layer
 set outputDirectory=%1
 echo %1
 copy dxrfallbackcompiler.dll %outputDirectory%
--- Samples/Desktop/D3D12Raytracing/tools/x64/PrebuildCheck.bat
+++ Samples/Desktop/D3D12Raytracing/tools/x64/PrebuildCheck.bat
@@ -1,23 +1,11 @@
 set FILENOTFOUND=0
 
-if not exist dxcompiler.dll (
-   echo No dxcompiler.dll found!
-   set FILENOTFOUND=1
-)
-
-if not exist dxc.exe (
-   echo No dxc.exe found!
-   set FILENOTFOUND=1
-)
-
 if not exist dxrfallbackcompiler.dll (
    echo No dxrfallbackcompiler.dll found!
    echo Note that if you were using an older version of the Raytracing samples, the Fallback Layer has been changed to require dxrfallbackcompiler.dll instead of dxcompiler.dll. This can be pulled from the below linked release
    set FILENOTFOUND=1
 )
 
-echo Renaming dxc->fxc so that Visual Studio can natively work with HLSL files and still compile them to SM 6.3
-copy dxc.exe fxc.exe
 
 if %FILENOTFOUND% NEQ 0 (
 	goto :mismatch
