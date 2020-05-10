0da8e15d0b211f2ab5abb9ebe3781a7ddd6ff040
James Stanard <jstanard@microsoft.com>
2020-05-10 08:58:29 -0700

    Shortened some very long shader names

--- Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingRealTimeDenoisedAmbientOcclusion/RTAO/Shaders/Denoising/EdgeStoppingFilter_Gaussian5x5CS.hlsl
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingRealTimeDenoisedAmbientOcclusion/RTAO/Shaders/Denoising/EdgeStoppingFilter_Gaussian5x5CS.hlsl
@@ -11,4 +11,4 @@
 
 #define HLSL
 #define GAUSSIAN_KERNEL_5X5
-#include "EdgeStoppingAtrousWaveletTransfromCrossBilateralFilterCS.hlsli"
+#include "AtrousWaveletTransfromCrossBilateralFilterCS.hlsli"
