240bc3acb3fd653cef4b9bea7b79a64755234456
amarpMSFT <amarp@microsoft.com>
2025-05-29 12:43:19 -0700

    merge two samples

---
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloShaderExecutionReordering/stdafx.h
@@ -0,0 +1,48 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently.
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <sstream>
+#include <iomanip>
+
+#include <list>
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include <assert.h>
+
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include "d3dx12.h"
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include "DXSampleHelper.h"
+#include "DeviceResources.h"
+
