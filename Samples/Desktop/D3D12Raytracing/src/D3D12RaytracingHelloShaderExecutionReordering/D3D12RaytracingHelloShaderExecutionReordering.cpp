eedf4ec3e9af629575de0654ca7a9a8c6fddb0c1
amarpMSFT <amarp@microsoft.com>
2025-05-29 11:40:16 -0700

    unneeded line

--- Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloShaderExecutionReordering/D3D12RaytracingHelloShaderExecutionReordering.cpp
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloShaderExecutionReordering/D3D12RaytracingHelloShaderExecutionReordering.cpp
@@ -157,7 +157,6 @@ D3D12RaytracingHelloShaderExecutionReordering::D3D12RaytracingHelloShaderExecuti
 void D3D12RaytracingHelloShaderExecutionReordering::OnInit()
 {
     std::vector<UUID> experimentalFeatures; 
-    experimentalFeatures.clear(); 
     experimentalFeatures.push_back(D3D12ExperimentalShaderModels); 
     ThrowIfFailed(D3D12EnableExperimentalFeatures((UINT)experimentalFeatures.size(), experimentalFeatures.data(), nullptr, nullptr));
     m_deviceResources = std::make_unique<DeviceResources>(
