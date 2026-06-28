686e35ae08cfebc6e92f490d4557395c2e8ef404
Wataru Kawamata <wkawamata0323@gmail.com>
2026-06-28 13:09:51 +0900

    Initialize unused texture descriptor slots
    refactoring

--- Samples/Desktop/D3D12HelloWorld/src/HelloTextureModified/TextureSemantic.h
+++ Samples/Desktop/D3D12HelloWorld/src/HelloTextureModified/TextureSemantic.h
@@ -18,4 +18,17 @@ enum class TextureSemantic : uint8_t
 
 static constexpr uint32_t kTextureSemanticCount = static_cast<uint32_t>(TextureSemantic::Count);
 
+inline const char* GetSemanticName(TextureSemantic semantic)
+{
+    switch (semantic)
+    {
+        case TextureSemantic::BaseColor:         return "BaseColor";
+        case TextureSemantic::MetallicRoughness: return "MetallicRoughness";
+        case TextureSemantic::Normal:            return "Normal";
+        case TextureSemantic::Occlusion:         return "Occlusion";
+        case TextureSemantic::Emissive:          return "Emissive";
+        default:                                 return "Unknown";
+    }
+}
+
 } // namespace Engine
